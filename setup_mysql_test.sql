23e1700e016704fd5fa1825b3c557f7128885c65
vicano-code <vc.anokwuru@gmail.com>
2024-04-16 13:19:52 -0700

    setup tests database

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- mySQL test database setup
+-- creates a test database hbnb_test_db
+-- creates a new user and set user password
+-- grants privileges to user
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test.* TO hbnb_test@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
